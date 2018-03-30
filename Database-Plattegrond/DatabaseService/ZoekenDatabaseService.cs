7e1c734ef704b40b01a7125bcc2a84056f2eea12
jeroenvdkolk <jeroenvdkolk@gmail.com>
2018-03-30 14:57:38 +0200

    zoek resultaten worden nu gesoorteerd op aantal hits

--- Database-Plattegrond/DatabaseService/ZoekenDatabaseService.cs
+++ Database-Plattegrond/DatabaseService/ZoekenDatabaseService.cs
@@ -13,18 +13,35 @@ namespace Database_Plattegrond.DatabaseService
         public ZoekViewModel GetAllSearchedDatasets(string zoekterm)
         {
             List<string> listZoekElements = zoekterm.Split(' ').ToList();
-
+            int counter = 0;
             SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ApplicatiePlattegrondConnectionString"].ToString());
             connection.Open();           
             SqlCommand command = new SqlCommand("", connection)
             {
-                CommandText = "SELECT * FROM dataset WHERE"
+                CommandText = "SELECT DISTINCT *, ("
             };
             
             foreach (string zoek in listZoekElements)
             {
-                command.CommandText = command.CommandText + " Zoektermen LIKE '%" + zoek + "%' OR Naam LIKE '%" + zoek + "%' OR beschrijving LIKE '%" + zoek + "%'";
-            }
+                counter++;                
+                command.CommandText = command.CommandText + "(LEN(zoektermen) - LEN(REPLACE(zoektermen, '" + zoek + "', ''))) + (LEN(naam) - LEN(REPlACE(naam, '" + zoek + "',''))) + (LEN(beschrijving) - LEN(REPLACE(beschrijving, '" + zoek + "','')))/COALESCE(NULLIF(LEN('" + zoek + "'), 0), 1)";
+                if (counter != listZoekElements.Count)
+                    command.CommandText = command.CommandText + " + ";
+                else
+                {
+                    counter = 0;
+                    command.CommandText = command.CommandText + ") as found FROM dataset WHERE ";
+                    foreach(string zoek2 in listZoekElements)
+                    {
+                        counter++;
+                        command.CommandText = command.CommandText + " Zoektermen LIKE '%" + zoek2 + "%' OR Naam LIKE '%" + zoek2 + "%' OR beschrijving LIKE '%" + zoek2 + "%'";
+                        if (counter != listZoekElements.Count)
+                            command.CommandText = command.CommandText + " OR";
+                        else
+                            command.CommandText = command.CommandText + "order by found desc";
+                    }
+                }
+            }            
             
             SqlDataReader reader = command.ExecuteReader();
             ZoekViewModel zoekVM = new ZoekViewModel { ZoekDatasets = new List<Dataset>() };
