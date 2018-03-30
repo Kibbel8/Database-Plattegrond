aa3aa32621866e2e61c74b663d088aa0ad3c0e5f
rickyvelis <37187134+rickyvelis@users.noreply.github.com>
2018-03-30 14:17:23 +0200

    Layout Details view aangepast, AanvraagForm view: info wordt dynamisch ingeladen

--- Database-Plattegrond/Controllers/DatasetsController.cs
+++ Database-Plattegrond/Controllers/DatasetsController.cs
@@ -36,7 +36,7 @@ namespace Database_Plattegrond.Controllers
             RelevantDatabaseService RDS = new RelevantDatabaseService();
             List<Relevant> links = RDS.GetRelevanteLinksVoorDataset(id.Value);
 
-            DatasetDetail datasetDetail = new DatasetDetail { Dataset = dataset, Comments = comments, Links = links };
+            DatasetDetail datasetDetail = new DatasetDetail { Dataset = dataset, Comments = comments, Links = links};
             return View(datasetDetail);
         }
 
@@ -90,6 +90,7 @@ namespace Database_Plattegrond.Controllers
 
             return View(datasetBewerken);
         }
+
         [HttpPost]
         public ActionResult DatasetBewerken(DatasetBewerken model)
         {
@@ -119,11 +120,21 @@ namespace Database_Plattegrond.Controllers
             return RedirectToAction("Details", new { id = id });
         }
 
-        public ActionResult AanvraagFormulier()
+        public ActionResult AanvraagFormulier(int? id = -1)
         {
             ViewBag.Message = "Aanvraag Formulier";
+            
+            DatasetsDatabaseService dds = new DatasetsDatabaseService();
+            Dataset dataset = dds.GetDatasetFromId(id.Value);
 
-            return View();
+            Aanvragen aanvragen = new Aanvragen
+            {
+                Naam = dataset.Naam,
+                Beschrijving = dataset.Beschrijving,
+                Eigenaar = dataset.Eigenaar
+            };
+
+            return View(aanvragen);
         }
     }
 }
---
+++ Database-Plattegrond/Models/Aanvragen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Database_Plattegrond.Models
+{
+    public class Aanvragen
+    {
+        public string Naam { get; set; }
+        public string Beschrijving { get; set; }
+        public Gebruiker Eigenaar { get; set; }
+    }
+}
