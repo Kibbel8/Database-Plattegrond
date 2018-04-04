aa76071672a5bba298a419be952e2a68e4de3f1a
rickyvelis <37187134+rickyvelis@users.noreply.github.com>
2018-04-04 09:42:32 +0200

    uhhh

---
+++ Database-Plattegrond/Models/Aanvraag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Database_Plattegrond.Models
+{
+    public class Aanvraag
+    {
+        public Dataset Dataset { get; set; }
+        public Gebruiker Eigenaar { get; set; }
+        public string Verwerkingsdoeleind { get; set; }
+        public string WanneerNodig { get; set; }
+    }
+}
